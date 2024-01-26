7b1f241fce022014308546dc147a93cc35ae2c26
alireza-webdeveloper <alirezakh0090@gmail.com>
2024-01-26 19:28:33 +0330

    (Add):<-Config Project->

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
 const utilsWorks = require('./utils/works');
 const express = require('express');
 const rootRouter = require('./router/root');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
 const categories = require('./router/categories');
 const mongoose = require('mongoose');
-const cors = require('cors')
+const cors = require('cors');
 const recipesRouter = require('./router/recipes');
 
-mongoose.connect("mongodb://127.0.0.1:27017/rApp")
-    .then(() => {
-        const app = express();
-        app.use(express.static('public'))
-        app.use(cors())
-        app.use(express.json());
+dotenv.config({ path: './config.env' });
 
-        // add routes
-        app.use('/', rootRouter);
-        app.use('/api/recipes', recipesRouter)
-        app.use('/api/categories', categories);
-        app.listen(process.env.PORT || 3000, process.env.HOST || 'localhost', () => {
-            console.log('Server Start');
-        });
-    })
-    .catch(err => {
-        console.log(err)
-    })
+mongoose
+  .connect(process.env.MONGOATLAS_URL, {
+    dbName: process.env.MONGO_ATLAS_DB,
+    user: process.env.MONGO_ATLAS_USERNAME,
+    pass: process.env.MONGO_ATLAS_PASSWORD,
+    writeConcern: { w: 'majority' },
+  })
+  .then(() => {
+    const app = express();
+    app.use(express.static('public'));
+    app.use(cors());
+    app.use(express.json());
+
+    // add routes
+    app.use(morgan('dev'));
+    app.use('/', rootRouter);
+    app.use('/api/recipes', recipesRouter);
+    app.use('/api/categories', categories);
+    app.listen(
+      process.env.PORT || 3000,
+      process.env.HOST || 'localhost',
+      () => {
+        console.log('Server Start');
+      }
+    );
+  })
+  .catch((err) => {
+    console.log(err);
+  });
